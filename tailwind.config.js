1d2a9f3a0ab6554517a3b2de90553cd0086fe271
Amritanshu Kumar <amritanshu8271@gmail.com>
2023-03-04 12:34:58 +0530

    AK

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,72 +1,36 @@
 module.exports = {
-  future:{
-    removeDeprecatedGapUtilities:true
+  future: {
+    removeDeprecatedGapUtilities: true
   },
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",module.exports = {
-        future: {
-          removeDeprecatedGapUtilities: true
-        },
-        purge: {
-          content: ['./src/**/*.js', './src/**/**/*.js']
-        },
-        theme: {
-          fill: (theme) => ({
-            red: theme('colors.red.primary')
-          }),
-          colors: {
-            white: '#ffffff',
-            blue: {
-              medium: '#005c98'
-            },
-            black: {
-              light: '#262626',
-              faded: '#00000059'
-            },
-            gray: {
-              base: '#616161',
-              background: '#fafafa',
-              primary: '#dbdbdb'
-            },
-            red: {
-              primary: '#ed4956'
-            }
-          }
-        },
-        variants: {
-          extend: {
-            display: ['group-hover']
-          }
-        }
-      };
-      
-    ],
-    theme: {
-      extend: {},
-      fill:(theme)=>({
-      red:theme('colors.red.primary')
-      }),
-      colors: {  
-        white:'#ffffff',
-        blue:{
-          medium:'#005c98'
-        },
-        black:{
-          light:'#262626',
-          faded:'00000059'
-        },
-        gray:{
-          base:'#616161',
-          backgtound:'#fafafa',
-            primary:'#dbdbdb'
-        },
-        red:{
-          primary:'#ed4956'
-        }
+  purge: {
+    content: ['./src/**/*.js', './src/**/**/*.js']
+  },
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
       }
-    },
-    variants:{
-      display:['group-hover']
-    },
-    plugins: [],
+    }
+  },
+  variants: {
+    extend: {
+      display: ['group-hover']
+    }
   }
+};

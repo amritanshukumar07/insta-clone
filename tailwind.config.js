ba27400ea875b184cceaa907ec8d450d33028f93
Amritanshu Kumar <amritanshu8271@gmail.com>
2023-03-04 12:30:35 +0530

    AK

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@ module.exports = {
     removeDeprecatedGapUtilities:true
   },
     content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
+      "./src/**/*.{js,jsx,ts,tsx}",module.exports = {
+        future: {
+          removeDeprecatedGapUtilities: true
+        },
+        purge: {
+          content: ['./src/**/*.js', './src/**/**/*.js']
+        },
+        theme: {
+          fill: (theme) => ({
+            red: theme('colors.red.primary')
+          }),
+          colors: {
+            white: '#ffffff',
+            blue: {
+              medium: '#005c98'
+            },
+            black: {
+              light: '#262626',
+              faded: '#00000059'
+            },
+            gray: {
+              base: '#616161',
+              background: '#fafafa',
+              primary: '#dbdbdb'
+            },
+            red: {
+              primary: '#ed4956'
+            }
+          }
+        },
+        variants: {
+          extend: {
+            display: ['group-hover']
+          }
+        }
+      };
+      
     ],
     theme: {
       extend: {},
